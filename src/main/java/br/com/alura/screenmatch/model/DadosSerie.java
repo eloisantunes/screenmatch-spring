875301041faa13bf6f9dd42fa383336d2975fb91
Eloísa Antunes <eloisantunesb@gmail.com>
2025-01-20 17:45:29 -0300

    modelando dados da serie

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -1,4 +1,11 @@
 package br.com.alura.screenmatch.model;
 
-public class DadosSerie {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+
 }
